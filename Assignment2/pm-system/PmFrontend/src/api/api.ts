f81291fa38c46003fa132e77b894c4a74d8f1fe7
Kuwar Jain <kuwarjain394@gmail.com>
2025-10-31 14:36:06 +0530

    added render url

--- Assignment2/pm-system/PmFrontend/src/api/api.ts
+++ Assignment2/pm-system/PmFrontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5094/api",
+  // baseURL: "http://localhost:5094/api",
+  baseURL: "https://pathlock-xnv8.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
